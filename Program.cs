4e2da6bded1ae99582a59929063e93545658193e
rehabx <36101622+rehabx@users.noreply.github.com>
2018-02-03 19:31:47 +0800

    logon

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace AOOADproject
 {
     class Program
     {
+        public static List<Employee> employeeList = new List<Employee>();
         public static List<JuniorAgent> juniorAgentList = new List<JuniorAgent>();
         public static List<Agent> agentList = new List<Agent>();
         public static List<SeniorAgent> SeniorAgentList = new List<SeniorAgent>();
@@ -16,21 +17,130 @@ namespace AOOADproject
         public static List<MedicalInsurance> medicalInsuranceList = new List<MedicalInsurance>();
         public static List<CarInsurance> carInsuranceList = new List<CarInsurance>();
 
+        public static string Logon()
+        {
+            string option = Console.ReadLine();
 
+            string userId;
+            bool checkUser = false;
+            while ((option != "1" || option != "2") && checkUser == false)
+            {
+                Console.WriteLine("Welcome to Provident Insurance! \n" +
+                                  "===================\n" +
+                                  "1. Log In to Customer\n" +
+                                  "2. Log In to Staff\n" +
+                                  "==================\n" +
+                                  "Please enter your option:");
+              
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Please enter your ID:");
+                        userId = Console.ReadLine();
+                        foreach (Employee employee in employeeList)
+                        {
+                            if (employee.Id.ToUpper() == userId.ToUpper())
+                            {
+                                Console.Write("Welcome " + employee.Name);
+                                checkUser = true;
+                                return "employee";
+                            }
+                        }
+                        if (checkUser == false)
+                        {
+                            Console.WriteLine("User does not exist, please try again.");
+                        }
+                        break;
+                    case "2":
+
+                        Console.WriteLine("Please enter your ID:");
+                        userId = Console.ReadLine();
+                        foreach (Employee employee in employeeList)
+                        {
+                            if (employee.Id.ToUpper() == userId.ToUpper())
+                            {
+                                Console.Write("Welcome " + employee.Name);
+                                checkUser = true;
+                                return "customer";
+                            }
+                        }
+                        if (checkUser == false)
+                        {
+                            Console.WriteLine("User does not exist, please try again.");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (option == "1" && checkUser == true)
+                { return "employee"; }
+
+                else if (option == "2" && checkUser == true )
+                { return "cusomter"; }
+            }
+
+            
+        }
 
         static void Main(string[] args)
         {
-
-
+            initialisation();
+            Logon();
+            
             Console.WriteLine("Welcome to Provident Insurance! \n" +
                               "===================\n" +
                               "1. Log In to Customer\n" +
                               "2. Log In to Staff\n" +
                               "==================\n" +
                               "Please enter your option");
-            int option = Convert.ToInt32(Console.ReadLine());
+            string option = Console.ReadLine();
 
-            if (option == 1)
+            string userId;
+            bool checkUser = false;
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Please enter your ID:");
+                    userId = Console.ReadLine();
+                    foreach (Employee employee in employeeList)
+                    {
+                        if (employee.Id.ToUpper() == userId.ToUpper())
+                        {
+                            Console.Write("Welcome " + employee.Name);
+                            checkUser = true;
+                            break;
+                        }
+                    }
+                    if (checkUser == false)
+                    {
+                        Console.WriteLine("User does not exist, please try again.");
+                    }
+                    break;
+                case "2":
+
+                    Console.WriteLine("Please enter your ID:");
+                    userId = Console.ReadLine();
+                    foreach (Employee employee in employeeList)
+                    {
+                        if (employee.Id.ToUpper() == userId.ToUpper())
+                        {
+                            Console.Write("Welcome " + employee.Name);
+                            checkUser = true;
+                            break;
+                        }
+                    }
+                    if (checkUser == false)
+                    {
+                        Console.WriteLine("User does not exist, please try again.");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+            
+            /*if (option == 1)
             {
                 Console.WriteLine("payment");
             }
@@ -55,18 +165,25 @@ namespace AOOADproject
                         Console.WriteLine("You have exited this thing");
                     }
                 }
-            }
+            }*/
         }
         public static void initialisation()
         {
 
             JuniorAgent ja = new JuniorAgent("Marcus", "1001", "JuniorAgent");
             juniorAgentList.Add(ja);
+            
             Agent ag = new Agent("Derrick", "2001", "Agent");
             agentList.Add(ag);
+            employeeList.Add(ag);
             SeniorAgent sa = new SeniorAgent("jennifer", "3001", "SeniorAgent");
             SeniorAgentList.Add(sa);
+            employeeList.Add(sa);
+            Administrator ad = new Administrator("Joshua", "4001", "Adminsitrator");
+            administratorList.Add(ad);
+            employeeList.Add(ad);
 
         }
+
     }
 }
